585f6a51b8c9eebd69a68bccf597c8a3cab0ee84
Alexander Gröttrup <alexander.groettrup@googlemail.com>
2021-03-24 00:34:33 +0100

    implement c api

---
+++ include/chucknorris.h
@@ -0,0 +1,20 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Preface every function with `chuck_norris` to create a pseude namespace */
+/* Use an opaque pointer to refer to ChuckNorris objects in C. 
+   typecasts have to be performed in C++. */ 
+typedef struct chuck_norris chuck_norris_t;
+
+/* explicit initialization and destruction in C: */
+chuck_norris_t* chuck_norris_init(void);
+void chuck_norris_deinit(chuck_norris_t*);
+
+/* C API cannot use methods but functions that refer to
+   a struct that belongs to an object. */
+char* chuck_norris_get_fact(chuck_norris_t*);
+
+#ifdef __cplusplus
+}
+#endif
